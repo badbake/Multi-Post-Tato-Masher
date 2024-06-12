5dc3cb5f07386510cb31827ec10a9e60066f0ea3
badbake <badbake89@gmail.com>
2024-06-11 23:34:57 -0400

    externalconfig
    take 2

--- BadBakes_Multi_Post-tato_Masher__V1.1.2_TESTNET.ps1
+++ BadBakes_Multi_Post-tato_Masher__V1.1.2_TESTNET.ps1
@@ -15,127 +15,17 @@ $WindowTitle = "Multi Post-tato Masher TESTNET"
 $host.ui.RawUI.WindowTitle = $WindowTitle
 $grpcurl = Join-Path -Path $PSScriptRoot -ChildPath "grpcurl.exe"
 
-# Define log levels for console and logfile (set to INFO by default, can be set to DEBUG, WARN, ERROR)
-$global:ConsoleLogLevel = "INFO"
-$global:FileLogLevel = "DEBUG"
+# Read configuration from config.ps1 file. Same directory as this script by default and logic.
+. ".\config.ps1"
 
 # Define Log Directory
 $logDirectory = ".\Logs"
 if (-not (Test-Path -Path $logDirectory)) {
     New-Item -ItemType Directory -Path $logDirectory
 }
-
-# Set Interval in seconds for Checking Post Service Status while running.
-$provingCheckInterval = 30
-
 $logFileName = "PostMasher$((Get-Date).ToString('MMddyyyy_HHmm')).txt"
 $logFilePath = Join-Path -Path $logDirectory -ChildPath $logFileName
 
-# Define configurations for each set of POST Data. 
-$instances = @{
-    "Post1" = @{									#Name of each instance must match the identity.key associaited with that POST data set. (Example - Post1 for use with Post1.key.) 
-        Arguments = @(
-            "--address=http://localhost:9094",		#Node's gRPC address. Ensure it matches the node's grpc-post-listener config option.
-            "--dir=./Post1",						#Post Data Directory, Set for each different set of Post Data.
-            "--operator-address=127.0.0.1:50051",	#Operator API
-            "--threads=1",							#Proving Options based on your hardware
-            "--nonces=64",							#Proving Options based on your hardware
-            "--randomx-mode=fast"					#Proving Options based on your hardware
-        )
-    }
-    "Post2" = @{									#Example - Post2 name for use with Post2.key
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post2",						#Set for Post DataDirectory 2
-            "--operator-address=127.0.0.1:50052",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post3" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post3",						
-            "--operator-address=127.0.0.1:50053",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post4" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post4",						
-            "--operator-address=127.0.0.1:50054",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post5" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post5",						
-            "--operator-address=127.0.0.1:50055",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post6" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post6",						
-            "--operator-address=127.0.0.1:50056",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post7" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post7",						
-            "--operator-address=127.0.0.1:50057",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post8" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post8",						
-            "--operator-address=127.0.0.1:50058",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post9" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post9",						
-            "--operator-address=127.0.0.1:50059",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-    "Post10" = @{									
-        Arguments = @(
-            "--address=http://localhost:9094",
-            "--dir=./Post10",						
-            "--operator-address=127.0.0.1:50060",
-            "--threads=1",
-            "--nonces=64",
-            "--randomx-mode=fast"
-        )
-    }
-
-    # Add/Remove Posts with names and arguments for all Post Services needed.
-}
 
 # Function to log messages with timestamp and log level
 function Log-Message {
@@ -153,7 +43,7 @@ function Log-Message {
     }
 
     # Only log messages that are equal to or higher than the current log level for file output
-    if ($logLevelHierarchy[$level] -ge $logLevelHierarchy[$global:FileLogLevel]) {
+    if ($logLevelHierarchy[$level] -ge $logLevelHierarchy[$FileLogLevel]) {
         $timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
         $logMessage = "$timestamp - [$level] - $message"
 
@@ -162,7 +52,7 @@ function Log-Message {
     }
 
     # Only log messages that are equal to or higher than the current log level for console output
-    if ($logLevelHierarchy[$level] -ge $logLevelHierarchy[$global:ConsoleLogLevel]) {
+    if ($logLevelHierarchy[$level] -ge $logLevelHierarchy[$ConsoleLogLevel]) {
         # Colorize and write the log message to the console
         Colorize-Logs -message $message -level $level -timestamp $timestamp
     }
