3b8aabec5cfc5990f7f54f68ab16c29a3a1876ff
badbake <badbake89@gmail.com>
2024-06-12 09:27:32 -0400

    Moved redundant
    Created a New Function 'Check-InstanceState' to combine redundant functionality from RunAllInstance and CheckandRunProvingInstances

--- BadBakes_Multi_Post-tato_Masher__V1.1.2_TESTNET.ps1
+++ BadBakes_Multi_Post-tato_Masher__V1.1.2_TESTNET.ps1
@@ -28,7 +28,6 @@ function Log-Message {
         [string]$level = "INFO"  # Default level is INFO for log file
     )
 
-    # Define the log level hierarchy
     $logLevelHierarchy = @{
         "DEBUG" = 1
         "INFO" = 2
@@ -36,24 +35,17 @@ function Log-Message {
         "ERROR" = 4
     }
 
-    # Only log messages that are equal to or higher than the current log level for file output
     if ($logLevelHierarchy[$level] -ge $logLevelHierarchy[$FileLogLevel]) {
         $timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
         $logMessage = "$timestamp - [$level] - $message"
-
-        # Write to the log file without colors
         $logMessage | Out-File -Append -FilePath $logFilePath
     }
 
-    # Only log messages that are equal to or higher than the current log level for console output
     if ($logLevelHierarchy[$level] -ge $logLevelHierarchy[$ConsoleLogLevel]) {
-        # Colorize and write the log message to the console
         Colorize-Logs -message $message -level $level -timestamp $timestamp
     }
 }
 
-
-# Function to display colorized logs in the console
 function Colorize-Logs {
     param (
         [string]$message,
@@ -89,7 +81,6 @@ function Colorize-Logs {
         }
     }
 
-    # Print the log message with color
     Write-Host -NoNewline -ForegroundColor $timestampColor $timestamp
     Write-Host -NoNewline " : "
     Write-Host -NoNewline -ForegroundColor $levelColor "[$level]"
@@ -116,12 +107,11 @@ function GetNumUnitsForInstance {
             return $null
         }
     } else {
-	Log-Message "Metadata file not found at ${metadataFilePath}" "ERROR"
+        Log-Message "Metadata file not found at ${metadataFilePath}" "ERROR"
         return $null
     }
 }
 
-# Function to calculate Proving %
 function Curl-ProvingProgress {
     param (
         [string]$operatorAddress,
@@ -130,7 +120,7 @@ function Curl-ProvingProgress {
 
     while ($true) {
         $response = Invoke-Expression ("curl http://$operatorAddress/status") 2>$null -ErrorAction Inquire
-		Log-Message " $($response) " "DEBUG"
+        Log-Message " $($response) " "DEBUG"
 
         if ($response -match "DoneProving") {
             Log-Message "Proving process completed" "INFO"
@@ -140,7 +130,8 @@ function Curl-ProvingProgress {
             return
         } elseif ($postStatus -match '^{.*}$') {
             $response | ConvertFrom-Json
-		}
+        }
+
         try {
             $jsonResponse = $response | ConvertFrom-Json
             if ($jsonResponse.Proving -and $jsonResponse.Proving.nonces) {
@@ -173,18 +164,13 @@ function Run-Instance {
         [string[]]$arguments
     )
 
-    # Extract directory from the arguments
     $dirArgument = ($arguments -like "--dir=*")[0]
     $instanceDir = $dirArgument.Split("=")[1]
-
-    # Call GetNumUnitsForInstance to get the NumUnits value
     $numUnits = GetNumUnitsForInstance -instanceDir $instanceDir
 
-    # Extract operator address from the arguments
     $operatorAddressArgument = ($arguments -like "--operator-address=*")[0]
     $operatorAddress = $operatorAddressArgument.Split("=")[1]
 
-    # Expected responses and Flags
     $idleResponse = '"state": "IDLE"'
     $provingResponse = '"state": "PROVING"'
     $previousState = ""
@@ -192,21 +178,18 @@ function Run-Instance {
     $idleFound = $false
     $provingStateReached = $false
     $shutdownInitiated = $false
-    $idleCounter = 0 # Initialize the idle counter
+    $idleCounter = 0
 
     # Log for service.exe
     $serviceLogFileName = "${instanceName}_service$((Get-Date).ToString('MMddyyyy_HHmm')).txt"
     $serviceLogFilePath = Join-Path -Path $logDirectory -ChildPath $serviceLogFileName
 
-    # Extract port number from the address argument
     $addressArgument = ($arguments -like "--address=*")[0]
     $port = $addressArgument.Split(":")[2].Trim("http://")
 
-    # Start Service with Arguments for Instance
     Log-Message "$instanceName is starting service.exe" "INFO"
     $serviceProcess = Start-Process -FilePath ".\service.exe" -ArgumentList $arguments -NoNewWindow -PassThru -RedirectStandardError $serviceLogFilePath
 
-    # Check if service process started successfully
     if ($serviceProcess -ne $null -and (Get-Process -Id $serviceProcess.Id -ErrorAction Inquire)) {
         Log-Message "$instanceName has successfully started PoST-Service." "INFO"
     } else {
@@ -219,13 +202,11 @@ function Run-Instance {
 
         $response = & "$grpcurl" --plaintext -d '{}' "localhost:$port" spacemesh.v1.PostInfoService.PostStates 2>&1
 
-        # Check if the response is empty or if there's an error
         if (-not $response) {
             Log-Message "No response received from gRPC call." "ERROR"
             return
         }
 
-        # Convert response to JSON
         try {
             $jsonResponse = $response | ConvertFrom-Json
         } catch {
@@ -233,16 +214,14 @@ function Run-Instance {
             return
         }
 
-        # Check if JSON conversion was successful
         if (-not $jsonResponse) {
             Log-Message "Failed to convert response to JSON." "ERROR"
             return
         }
 
-        # Now continue with processing the JSON response
         foreach ($state in $jsonResponse.states) {
             Log-Message "Found '$($state.name)' with state '$($state.state)'." "DEBUG"
-            if ($state.name -eq "$instanceName.key") {  # Check if the name exactly matches the instance name with ".key" suffix
+            if ($state.name -eq "$instanceName.key") {
                 Log-Message "'$instanceName' matched in the response." "DEBUG"
                 if ($state.state -eq "PROVING") {
                     $provingFound = $true
@@ -258,7 +237,7 @@ function Run-Instance {
             Log-Message "PoST-Service '$instanceName' is PROVING." "INFO"
             $provingStateReached = $true
             $previousState = "PROVING"
-            $idleCounter = 0 # Reset idle counter
+            $idleCounter = 0
         } elseif ($shutdownInitiated -eq $true) {
             Log-Message "Node returning idle for '$instanceName'. Proof is assumed accepted" "INFO"
             Stop-PoST-Service -process $serviceProcess
@@ -270,9 +249,9 @@ function Run-Instance {
         } elseif ($idleFound -and $previousState -ne "IDLE" -and -not $provingStateReached) {
             Log-Message "PoST-Service '$instanceName' is in the IDLE state." "INFO"
             $previousState = "IDLE"
-            $idleCounter = 0 # Reset idle counter
+            $idleCounter = 0
         } elseif ($idleFound -and $previousState -eq "IDLE") {
-            $idleCounter++ # Increment idle counter
+            $idleCounter++
             Log-Message "PoST-Service '$instanceName' continues to be in the IDLE state. Idle count: $idleCounter" "INFO"
             if ($idleCounter -ge 4) {
                 $shutdownInitiated = $true
@@ -292,9 +271,8 @@ function Stop-PoST-Service {
     )
 
     try {
-        # Send a termination signal and wait for the process to exit
         $retryCount = 3
-        $retryInterval = 5000  # 5 seconds
+        $retryInterval = 5000
 
         for ($i = 0; $i -lt $retryCount; $i++) {
             if ($process.HasExited) {
@@ -308,7 +286,6 @@ function Stop-PoST-Service {
             Start-Sleep -Milliseconds $retryInterval
         }
 
-        # If the process is still not exited, forcefully terminate it
         if (-not $process.HasExited) {
             Log-Message "PoST-Service did not exit within the timeout period. Forcing termination." "WARN"
             $process.Kill()
@@ -321,6 +298,43 @@ function Stop-PoST-Service {
     }
 }
 
+function Check-InstanceState {
+    param (
+        [PSCustomObject]$instance,
+        [string]$instanceName
+    )
+
+    try {
+        $addressArgument = ($instance.Arguments -like "--address=*")[0]
+        $port = $addressArgument.Split(":")[2].Trim("http://")
+        $response = & "$grpcurl" --plaintext -d '{}' "localhost:$port" spacemesh.v1.PostInfoService.PostStates 2>&1
+
+        if (-not $response) {
+            Log-Message "No response received from gRPC call." "ERROR"
+            return $false
+        }
+
+        $jsonResponse = $response | ConvertFrom-Json
+
+        if (-not $jsonResponse) {
+            Log-Message "Failed to convert response to JSON." "ERROR"
+            return $false
+        }
+
+        foreach ($state in $jsonResponse.states) {
+            Log-Message "Found '$($state.name)' with state '$($state.state)'." "DEBUG"
+            if ($state.name -eq "$instanceName.key") {
+                Log-Message "Instance name '$instanceName' matched in the response." "DEBUG"
+                if ($state.state -eq "PROVING") {
+                    return $true
+                }
+            }
+        }
+    } catch {
+        Log-Message "Error occurred while checking state for instance '$instanceName': $_" "ERROR"
+    }
+    return $false
+}
 
 # Function to run all instances sequentially
 function Run-AllInstances {
@@ -329,70 +343,23 @@ function Run-AllInstances {
         Run-Instance -instanceName $instanceName -arguments $instance.Arguments
     }
 
-    # Check if any instances are still in PROVING state and run them again
     $instancesInProvingState = $false
-	Log-Message "All Instances Ran. Re-Checking all Instances for PROVING state." "INFO"
+    Log-Message "All Instances Ran. Re-Checking all Instances for PROVING state." "INFO"
 	
     foreach ($instanceName in $instances.Keys) {
         $instance = $instances[$instanceName]
-		
-		# Display Message
-		Log-Message "Checking State of '$instanceName'." "DEBUG"
+        Log-Message "Checking State of '$instanceName'." "DEBUG"
 
-        try {
-            # Extract port number from the address argument
-            $addressArgument = ($instance.Arguments -like "--address=*")[0]
-            $port = $addressArgument.Split(":")[2].Trim("http://")
-
-            # Perform gRPC call to check the state
-            $response = & "$grpcurl" --plaintext -d '{}' "localhost:$port" spacemesh.v1.PostInfoService.PostStates 2>&1
-
-            # Check if the response is empty or if there's an error
-            if (-not $response) {
-                Log-Message "No response received from gRPC call." "ERROR"
-                continue
-            }
-
-            # Convert response to JSON
-            try {
-                $jsonResponse = $response | ConvertFrom-Json
-            } catch {
-                Log-Message "Failed to convert response to JSON: $_" "ERROR"
-                continue
-            }
-
-            # Check if JSON conversion was successful
-            if (-not $jsonResponse) {
-                Log-Message "Failed to convert response to JSON." "ERROR"
-                continue
-            }
-
-            # Now continue with processing the JSON response
-            foreach ($state in $jsonResponse.states) {
-                Log-Message "Found '$($state.name)' with state '$($state.state)'." "DEBUG"
-                if ($state.name -eq "$instanceName.key") {  # Check if the name exactly matches the instance name with ".key" suffix
-                    Log-Message "Instance name '$instanceName' matched in the response." "DEBUG"
-                    if ($state.state -eq "PROVING") {
-                        $instancesInProvingState = $true
-                        Log-Message "PROVING state found. Running PoST-Service for '$instanceName'." "INFO"
-                        Run-Instance -instanceName $instanceName -arguments $instance.Arguments
-                    } elseif ($state.state -eq "IDLE") {
-                        Log-Message "'$instanceName' shows IDLE." "DEBUG"
-                    } else {
-                        Log-Message "Unknown state for instance '$instanceName'." "WARN"
-                    }
-                    break
-                }
-            }
-        }
-        catch {
-            Log-Message "Error occurred while checking state for instance '$instanceName': $_" "ERROR"
+        if (Check-InstanceState -instance $instance -instanceName $instanceName) {
+            $instancesInProvingState = $true
+            Log-Message "PROVING state found. Running PoST-Service for '$instanceName'." "INFO"
+            Run-Instance -instanceName $instanceName -arguments $instance.Arguments
         }
     }
-    # If no instances requiring proof were found after running and checking again. Proceed.
+
     if (-not $instancesInProvingState) {
         Log-Message "All PoST-Service's showing IDLE." "INFO"
-		Log-Message "All PoST-Service's have completed proving." "INFO"
+        Log-Message "All PoST-Service's have completed proving." "INFO"
     }
 }
 
@@ -400,18 +367,12 @@ function Run-AllInstances {
 
 # Function to calculate the next trigger time based on the user's local time zone
 function Calculate-NextTriggerTime {
-    # Define the initial trigger date and time in UTC
     $initialTriggerDateTimeUtc = [DateTime]::new(2024, 6, 6, 23, 00, 0) #testnet12
 	#$initialTriggerDateTimeUtc = [DateTime]::new(2024, 5, 12, 20, 00, 0) #mainnet
-
-    # Convert the initial trigger time to the local time zone
     $initialTriggerDateTimeLocal = $initialTriggerDateTimeUtc.ToLocalTime()
-    # Get the current date and time in the local time zone
     $currentDateTimeLocal = Get-Date
 
-    # Check if the current date and time is past the initial trigger time
     if ($currentDateTimeLocal -gt $initialTriggerDateTimeLocal) {
-        # Calculate the time difference between the current time and the initial trigger time
         $timeDifference = $currentDateTimeLocal - $initialTriggerDateTimeLocal
         # Calculate the number of full 1-day intervals that have passed
         $fullIntervals = [Math]::Floor($timeDifference.TotalDays) #testnet12
@@ -420,13 +381,10 @@ function Calculate-NextTriggerTime {
         $nextTriggerDateTimeLocal = $initialTriggerDateTimeLocal.AddDays($fullIntervals + 1) #testnet12
 		#$nextTriggerDateTimeLocal = $initialTriggerDateTimeLocal.AddDays(($fullIntervals + 1) * 14) #mainnet
     } else {
-        # If the current time is before the initial trigger time, the next trigger time is the initial trigger time
         $nextTriggerDateTimeLocal = $initialTriggerDateTimeLocal
     }
 
-    # Log the next trigger date and time
     Log-Message "Next Cycle Gap: $($nextTriggerDateTimeLocal.ToString('MM/dd/yyyy hh:mm:ss tt'))" "INFO"
-    # Return the next trigger date and time
     return $nextTriggerDateTimeLocal
 }
 
@@ -439,25 +397,20 @@ function Update-ConsoleWithRemainingTime {
     while ($true) {
         $timeDifference = $nextTriggerTime - (Get-Date)
 
-        # Calculate remaining time in days, hours, minutes, and seconds
         $remainingDays = [Math]::Floor($timeDifference.TotalDays)
         $remainingHours = $timeDifference.Hours
         $remainingMinutes = $timeDifference.Minutes
         $remainingSeconds = $timeDifference.Seconds
 
-        # Format the remaining time
         $formattedRemainingTime = 'Days={0} Hours={1:00} Minutes={2:00} Seconds={3:00}' -f $remainingDays, $remainingHours, $remainingMinutes, $remainingSeconds
 
-
-        # Update console with the remaining time
         Write-Host -NoNewline "`r                             - Time Remaining: $formattedRemainingTime"
-        Start-Sleep -Seconds 1  # Update every second
+        Start-Sleep -Seconds 1
 
-        # Exit the loop when the time difference is less than or equal to zero
         if ($timeDifference.TotalSeconds -le 1) {
             Write-Host
-			Log-Message "Running POST Services" "INFO"
-			break
+            Log-Message "Running POST Services" "INFO"
+            break
         }
     }
 }
@@ -470,73 +423,23 @@ function Wait-ForTrigger {
 
         Log-Message "Waiting until PoEt Cycle Gap..." "INFO"
 
-        # Update the console with the remaining time until the next trigger
         Update-ConsoleWithRemainingTime -nextTriggerTime $nextTriggerTime
 
-        # Trigger all instances once the timer reaches zero
         Run-AllInstances
     }
 }
 
-# Function to check for PROVING states and run corresponding instances
 function Check-And-Run-ProvingInstances {
     $provingInstancesFound = $false
     
     foreach ($instanceName in $instances.Keys) {
         $instance = $instances[$instanceName]
-		
-		# Display Message
-		Log-Message "Checking State of '$instanceName'." "INFO"
+        Log-Message "Checking State of '$instanceName'." "INFO"
 
-        try {
-            # Extract port number from the address argument
-            $addressArgument = ($instance.Arguments -like "--address=*")[0]
-            $port = $addressArgument.Split(":")[2].Trim("http://")
-
-            # Perform gRPC call to check the state
-            $response = & "$grpcurl" --plaintext -d '{}' "localhost:$port" spacemesh.v1.PostInfoService.PostStates 2>&1
-
-            # Check if the response is empty or if there's an error
-            if (-not $response) {
-                Log-Message "No response received from gRPC call." "ERROR"
-                continue
-            }
-
-            # Convert response to JSON
-            try {
-                $jsonResponse = $response | ConvertFrom-Json
-            } catch {
-                Log-Message "Failed to convert response to JSON: $_" "ERROR"
-                continue
-            }
-
-            # Check if JSON conversion was successful
-            if (-not $jsonResponse) {
-                Log-Message "Failed to convert response to JSON." "ERROR"
-                continue
-            }
-
-            # Now continue with processing the JSON response
-            foreach ($state in $jsonResponse.states) {
-                Log-Message "Found '$($state.name)' with state '$($state.state)'." "DEBUG"
-                if ($state.name -eq "$instanceName.key") {  # Check if the name exactly matches the instance name with ".key" suffix
-                    Log-Message "Instance name '$instanceName' matched in the response." "DEBUG"
-                    if ($state.state -eq "PROVING") {
-                        $provingInstancesFound = $true
-                        Log-Message "PROVING state found. Running PoST-Service for '$instanceName'." "INFO"
-                        Run-Instance -instanceName $instanceName -arguments $instance.Arguments
-                    } elseif ($state.state -eq "IDLE") {
-                        Log-Message "'$instanceName' shows IDLE." "DEBUG"
-                    } else {
-                        Log-Message "Unknown state for instance '$instanceName'." "WARN"
-                    }
-                    # Break out of the loop once the correct instance is found
-                    break
-                }
-            }
-        }
-        catch {
-            Log-Message "Error occurred while checking state for instance '$instanceName': $_" "ERROR"
+        if (Check-InstanceState -instance $instance -instanceName $instanceName) {
+            $provingInstancesFound = $true
+            Log-Message "PROVING state found. Running PoST-Service for '$instanceName'." "INFO"
+            Run-Instance -instanceName $instanceName -arguments $instance.Arguments
         }
     }
     
@@ -545,9 +448,6 @@ function Check-And-Run-ProvingInstances {
         Log-Message "No PoST Services found requiring proof, proceeding with timer..." "INFO"
     }
 }
-
-
-
 
 # Main entry point
 Check-And-Run-ProvingInstances
