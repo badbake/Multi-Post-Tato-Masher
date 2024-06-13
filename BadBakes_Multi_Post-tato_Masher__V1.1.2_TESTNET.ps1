39dbab86721ae0a2df2d4bf87b59bb264bc69f48
badbake <badbake89@gmail.com>
2024-06-12 20:42:15 -0400

    Update BadBakes_Multi_Post-tato_Masher__V1.1.2_TESTNET.ps1

--- BadBakes_Multi_Post-tato_Masher__V1.1.2_TESTNET.ps1
+++ BadBakes_Multi_Post-tato_Masher__V1.1.2_TESTNET.ps1
@@ -263,7 +263,7 @@ function Run-Instance {
         } elseif ($idleFound -and $previousState -eq "IDLE") {
             $idleCounter++
             Log-Message "PoST-Service '$instanceName' continues to be in the IDLE state. Idle count: $idleCounter" "INFO"
-            if ($idleCounter -ge 2) {
+            if ($idleCounter -ge 4) {
                 $shutdownInitiated = $true
                 Log-Message "PoST-Service '$instanceName' idle state detected $idleCounter times. Initiating shutdown." "INFO"
                 Stop-PoST-Service -process $serviceProcess
